a79b49ab7176209aed3483871cb5cb1ca9393d0e
emilnorsker <56673512+emilnorsker@users.noreply.github.com>
2020-05-11 12:09:22 +0200

    0.0

---
+++ src/main/java/rme/demo/NordicMoterhomeApplication.java
@@ -0,0 +1,13 @@
+package rme.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordicMoterhomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NordicMoterhomeApplication.class, args);
+    }
+
+}
