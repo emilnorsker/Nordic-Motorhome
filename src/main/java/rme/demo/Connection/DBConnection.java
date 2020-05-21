1aef8692772f79a799bf8d9d29d96561c5f2b5f0
RasmusKW <rasmus_w@hotmail.dk>
2020-05-22 00:51:53 +0200

    RKW | 22/05 | Added Interfaces and DBConnection

---
+++ src/main/java/rme/demo/Connection/DBConnection.java
@@ -0,0 +1,33 @@
+package rme.demo.Connection;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnection {
+
+    private static Connection conn;
+
+    public static Connection getDatabaseConnection() throws ClassNotFoundException {
+
+        if (conn != null) return conn;
+
+        String name, pass, url;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            url = "jdbc:mysql://den1.mysql5.gear.host:3306/motorhomedb";
+            name = "motorhomedb";
+            pass = "Tw00?vT9X?C9";
+            conn = DriverManager.getConnection(url, name, pass);
+            System.out.println("Connection created");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+        return conn;
+    }
+}
---
+++ src/main/java/rme/demo/Repository/interfaces/MotorhomeRepo.java
@@ -0,0 +1,14 @@
+/**
+ * @author Rasmus Wedelheim
+ */
+package rme.demo.Repository.interfaces;
+
+import rme.demo.Connection.DBConnection;
+
+import java.sql.Connection;
+
+public interface MotorhomeRepo {
+
+    int Search (int id);
+
+}
