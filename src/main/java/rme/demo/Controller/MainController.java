f574f02a110126112115a40da6e37b0687f8d638
RasmusKW <rasmus_w@hotmail.dk>
2020-05-21 10:30:03 +0200

    Revert "Revert "Initial Setup""
    This reverts commit 06a564d6f283082033db26a09da3c640999afd0e.

---
+++ src/main/java/rme/demo/Controller/MainController.java
@@ -0,0 +1,14 @@
+package rme.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
+    }
+}
