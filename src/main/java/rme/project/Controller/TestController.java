0cde455e66313294df8d0aae3de7fb27ce8d4642
emilnorsker <56673512+emilnorsker@users.noreply.github.com>
2020-05-25 14:36:18 +0200

    EGN | 25/05 | VC 0.2

---
+++ src/main/java/rme/project/Controller/TestController.java
@@ -0,0 +1,19 @@
+package rme.project.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+
+@Controller
+public class TestController
+{
+    @GetMapping("/test")
+    public String test(Model model)
+    {
+        model.addAttribute("map", new HashMap<String, String>().put("key", "value"));
+        model.addAttribute("bool", false);
+        return "test";
+    }
+}
