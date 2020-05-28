32af4927be1771ab0dd1fbf4535739f6c10dab20
emilnorsker <56673512+emilnorsker@users.noreply.github.com>
2020-05-28 11:58:32 +0200

    EGN | 26/05 | update 1.5

--- src/main/java/rme/project/Controller/MotorhomeController.java
+++ src/main/java/rme/project/Controller/MotorhomeController.java
@@ -2,22 +2,20 @@ package rme.project.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import rme.project.Models.Motorhome;
 import rme.project.Repository.implementations.MotorhomeRepoIMPL;
 import java.sql.SQLException;
 
 @Controller
+@RequestMapping(("/motorhomes"))
 public class MotorhomeController {
     private MotorhomeRepoIMPL motorRepo;
 
     {
         motorRepo = new MotorhomeRepoIMPL();
     }
-    @GetMapping("/motorhomes")
+    @GetMapping("")
     public String motorhomes(Model model){
         model.addAttribute("motorhomes", motorRepo.readAll());
         return "motorhomes";
