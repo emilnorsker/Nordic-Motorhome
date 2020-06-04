52e2398923fea5178c6004c86e5b566dde559aef
RasmusKW <rasmus_w@hotmail.dk>
2020-06-04 04:42:09 +0200

    RKW | 04/06 | Final Comments

--- src/main/java/rme/project/Util/DBConnection.java
+++ src/main/java/rme/project/Util/DBConnection.java
@@ -13,7 +13,18 @@ import java.util.Properties;
 
 public class DBConnection {
 
+    /**
+     * @author Mikkel Åxman
+     * Class for establishing the Database connection using DriverManager
+     * @param conn           variable later made as a singleton pattern in the if statement
+     * @param url            String used to hold the URL for the Database
+     * @param name           String used to hold the Username for the Database
+     * @param pass           String used to hold the Password for the Database
+     * @return
+     */
+
     private static Connection conn;
+
 
     public static Connection getDatabaseConnection()
     {
@@ -21,6 +32,7 @@ public class DBConnection {
         if (conn != null) return conn;
 
         String name, pass, url;
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             url = "jdbc:mysql://den1.mysql5.gear.host:3306/motorhomedb?autoReconnect=true";
